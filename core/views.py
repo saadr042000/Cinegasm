a338aebc0b337d25f81781c83b4d83977b3c97a2
Saad Rahman <saadrahman242@gmail.com>
2023-08-18 15:58:59 -0400

    Added User Account Settings

--- core/views.py
+++ core/views.py
@@ -10,6 +10,35 @@ from .models import Profile
 def index(request):
     return render(request, 'index.html')
 
+
+@login_required(login_url='signin')
+def settings(request):
+    user_profile = Profile.objects.get(user=request.user)
+
+    if request.method == 'POST':
+        
+        if request.FILES.get('image')== None:
+            image = user_profile.profileimg
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        if request.FILES.get('image')!= None:
+            image = request.FILES.get('image')
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        return redirect('settings')
+
+    return render(request, 'setting.html', {'user_profile': user_profile})
+
 def signup(request):
     if request.method == 'POST':
         username = request.POST['username']
@@ -28,10 +57,15 @@ def signup(request):
                 user = User.objects.create_user(username=username, email=email, password=password)
                 user.save()
 
+                # log user in and redirect to settings page
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
+
+                # create a profile object for the new user
                 user_model = User.objects.get(username=username)
                 new_profile = Profile.objects.create(user= user_model, id_user = user_model.id)
                 new_profile.save()
-                return redirect('signup')
+                return redirect('setting')
 
         else:
             messages.info(request, 'Password does not match')
